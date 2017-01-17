371709afcd3ca36aed39b97c73a6b400eea7aee7
yaosixu <jason_yaosixu@163.com>
2017-01-17 15:33:48 +0800

    first commit

---
+++ YSXChooseTime_Swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  YSXChooseTime_Swift
+//
+//  Created by yaosixu on 2017/1/17.
+//  Copyright © 2017年 Jason_Yao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
